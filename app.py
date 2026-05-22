8e94986a2741cc27067e92a54067fdd6700796b3
Muhamad Ghandi Nur Setiawan <134348061+GhandiZero0X@users.noreply.github.com>
2026-05-23 02:46:40 +0700

    [+] Update app.py minor

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ from routes.routes import routes_bp
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-app = Flask(__name__)
-app.secret_key = "sentimen-saham-secret-key"
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "sentimen-saham-secret-key"
 
-# Daftarkan blueprint
-app.register_blueprint(routes_bp)
+    # Daftarkan blueprint
+    app.register_blueprint(routes_bp)
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True, port=5000)
     app.run(host="0.0.0.0", port=5000, debug=True)
