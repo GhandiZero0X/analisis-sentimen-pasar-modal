a1407dd469fb623010549d2453a3c4fe7eccb29f
Muhamad Ghandi Nur Setiawan <134348061+GhandiZero0X@users.noreply.github.com>
2026-05-23 02:42:16 +0700

    [+] Update app.py minor start

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # app.py
+import os
+import sys
+
 from flask import Flask
 from routes.routes import routes_bp
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(__name__)
 app.secret_key = "sentimen-saham-secret-key"
@@ -10,3 +15,4 @@ app.register_blueprint(routes_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
